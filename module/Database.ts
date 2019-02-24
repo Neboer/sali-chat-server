511e61c1f7d32f22d15253ccc397cde629418850
Neboer <43609792+Neboer@users.noreply.github.com>
2019-02-24 17:54:01 +0800

    3.0版本服务端，配合4.0版本客户端 增加登录注册功能，接入数据库 改用yarn作为包管理工具

---
+++ module/Database.ts
@@ -0,0 +1,65 @@
+import Sequelize = require("sequelize");
+import * as Database from '../Database.json'
+// import {where} from "sequelize";
+
+const seq = new Sequelize(Database.database, Database.username, Database.password, {
+    host: Database.host,
+    dialect: Database.dialect,
+    operatorsAliases: false,
+    logging: true
+});
+
+const userTable = seq.define('user', {
+    username: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.STRING(255)
+    }
+}, {
+    freezeTableName: true,
+    tableName: 'users',
+    timestamps: false
+});
+
+export function insertUser(username, password, onSuccess) {
+    userTable.sync().then(() => {
+        return userTable.create({
+            username: username,
+            password: password
+        });
+    }).then(() => onSuccess());
+}
+
+export function queryUser(username, password, onUserNotExist, onWrongPassword, onVerify) {
+    userTable.findAll({
+        where: {
+            username: username
+        }
+    }).then((hosts: any[]) => {
+        const host = hosts[0];
+        if (host === undefined) {
+            onUserNotExist();
+        } else if (host.dataValues.password !== password) {
+            onWrongPassword();
+        } else {
+            onVerify();
+        }
+    })
+}
+
+export function queryUserExistence(username, onUserNotExist, onUserExist) {
+    userTable.findAll({
+        where: {
+            username: username
+        }
+    }).then((hosts: any[]) => {
+            if (hosts.length === 0) {
+                onUserNotExist()
+            } else {
+                onUserExist()
+            }
+        }
+    )
+}
--- server.ts
+++ server.ts
@@ -1,39 +1,54 @@
 // 服务器入口，程序主逻辑
 import http = require('http');
-import {Server} from 'ws'
-// import ClientCommand from './module/ClientCommand'
+import {Server} from 'ws';
+import {insertUser, queryUser, queryUserExistence} from "./module/Database";
 import Message from './module/Message'
 import {parse, postSender, userList} from "./module/Post"
 import ServerCommand from './module/ServerCommand'
 import ServerUser from './module/ServerUser'
 import User from "./module/User";
 
-const httpServer = http.createServer((request, response) => {
+http.createServer((request, response) => {
     if (request.method === 'POST') {// verify form data
         let data = '';
         request.on('data', (chunk => {
             data += chunk
         }));
         request.on('end', () => {
+            const a = JSON.parse(data);
             response.writeHead(200, {
                 'Access-Control-Allow-origin': '*'
             });
             switch (request.url) {
                 case "/login":
-                    postSender(response, 'ok');
+                    queryUser(a.username, a.password,
+                        () => postSender(response, 'no user'),
+                        () => postSender(response, 'wrong password'),
+                        () => postSender(response, 'ok')
+                    );
                     break;
                 case "/register":
-                    postSender(response, 'ok');
+                    queryUserExistence(a.username,
+                        () => {
+                            insertUser(a.username, a.password,
+                                () => postSender(response, 'ok')
+                            );
+                        },
+                        () => postSender(response, 'no')
+                    );
                     break;
                 case "/attempt":
-                    postSender(response, 'ok');
+                    queryUserExistence(a.username,
+                        () => postSender(response, 'ok'),// 用户名不存在，返回ok准许注册
+                        () => postSender(response, 'no')// 用户名存在，返回no禁止注册
+                    );
                     break;
             }
         });
     }
-    if (request.method === 'GET'){
-        response.writeHead(200,{'Access-Control-Allow-origin': '*'});
-        postSender(response,userList.toOnlineString())
+    if (request.method === 'GET') {
+        response.writeHead(200, {'Access-Control-Allow-origin': '*'});
+        postSender(response, userList.toOnlineString())
     }
     if (request.method === 'OPTIONS') {
         response.writeHead(200, {
@@ -65,10 +80,10 @@ wsServer.on('connection', (socket, req) => {// 服务器与客户端之间传递
         userList.broadcast(comeMessage)
     });
     socket.on("close", () => {
-            const logoutUser:ServerUser|null = userList.findUserBySocket(socket);
-            if(logoutUser!==null){
+            const logoutUser: ServerUser | null = userList.findUserBySocket(socket);
+            if (logoutUser !== null) {
                 logoutUser.logout();
-                userList.broadcast(new ServerCommand('logout',{user:logoutUser.toUser()}))
+                userList.broadcast(new ServerCommand('logout', {user: logoutUser.toUser()}))
             } else {
                 console.error("warning,unauthorized logout")
             }
